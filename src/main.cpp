197c7e09a12ef7da1b263c6cdda9903339a8c86a
MontenegroDE <davidmontenegrov@gmail.com>
2025-04-03 23:18:30 -0500

    parte del contador de 0 al 99 completadad

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,21 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main(void) {
-    DDRD|=0xF0;//Pines del 4 al 7
-    int unidades=0;
 
-    while (1) {
-        PORTD=(PORTD&0x0F)|((unidades<<4)&0xF0);//mantiene el uso de D4 a D7
-        _delay_ms(1000);
+int main(void){
+    DDRD=0xFF;
+    int unidades=0;
+    int decenas=0;
+    while(1){
+        PORTD=((unidades<<4)&0xF0)|(decenas&0x0F); //definir 0 al 3 para decenas y del 4 al 7 como unidades
+        _delay_ms(500);
         unidades++;
-        if (unidades>9){
-            unidades=0;//incrementa y reinicia al pasar 9
+        if(unidades>9){
+            unidades=0;
+            decenas++; //incrementa las decenas si pas del 9
+            if(decenas>9){
+                decenas=0; //reiniciar al pasar de 99
+            }
         }
     }
 }
